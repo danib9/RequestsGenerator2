80e9c1aa43f9d78a59e4c9ac41758a50ad6a7128
danib9 <danielb@365scores.com>
2025-11-06 17:05:51 +0200

    Updated requestTypesConfig.ts

--- src/config/requestTypesConfig.ts
+++ src/config/requestTypesConfig.ts
@@ -255,15 +255,15 @@ export const sharedParameters: RequestParameter[] = [
       { value: 'Chat', label: 'Chat' },
       { value: 'Transfers', label: 'Transfers' },
       { value: 'SQUAD', label: 'Squad' },
-      { value: 'Highlights', label: 'Highlights' }
-      { value: 'Scores', label: 'Scores' }
-      { value: 'Buzz', label: 'Buzz' }
-      { value: 'Brackets', label: 'Brackets' }
-      { value: 'CompetitionsStats', label: 'Competitions Stats' }
-      { value: 'Videos', label: 'Videos' }
-      { value: 'Teams', label: 'Teams' }
-      { value: 'History', label: 'History' }
-      { value: 'DrawingOpponents', label: 'Drawing Opponents' },
+      { value: 'Highlights', label: 'Highlights' },
+      { value: 'Scores', label: 'Scores' },
+      { value: 'Buzz', label: 'Buzz' },
+      { value: 'Brackets', label: 'Brackets' },
+      { value: 'CompetitionsStats', label: 'Competitions Stats' },
+      { value: 'Videos', label: 'Videos' },
+      { value: 'Teams', label: 'Teams' },
+      { value: 'History', label: 'History' },
+      { value: 'DrawingOpponents', label: 'Drawing Opponents' }
     ]
   },
   { 
